f8324f9cc7b70f227585533f4d6b6637046d3dc2
Ba Duy <nguyenvubaduy9a11a@gmail.com>
2024-11-03 22:53:38 +0700

    design register page - register user

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,8 +1,125 @@
+import { Button, Col, Divider, Form, Input, notification, Row } from "antd";
+import { Link, useNavigate } from "react-router-dom";
+import { registerUserAPI } from "../services/api.service";
 
 const RegisterPage = () => {
+    const [form] = Form.useForm();
+    const navigate = useNavigate()
+
+    const handleSubmitRegister = async (values) => {
+        const { fullName, email, password, phone } = values
+        const res = await registerUserAPI(fullName, email, password, phone)
+        if (res.data) {
+            notification.success({
+                message: "Register User",
+                description: "Register User Successful!"
+            })
+            navigate("/login")
+        } else {
+            notification.error({
+                message: "Register User",
+                description: JSON.stringify(res.message)
+            })
+        }
+    }
+
     return (
-        <div>Register Page</div>
+        <Form
+            form={form}
+            onFinish={handleSubmitRegister}
+            layout="vertical"
+            style={{ margin: "30px" }}
+            autoComplete="off"
+        >
+
+            <h2 style={{ textAlign: "center" }}>Sign Up</h2>
+            <Row justify={"center"}>
+                <Col xs={24} md={8}>
+                    <Form.Item
+                        name="fullName"
+                        label="Full name"
+                        rules={[
+                            {
+                                required: true,
+                                message: "Please enter your full name!"
+                            },
+                        ]}
+                    >
+                        <Input />
+                    </Form.Item>
+                </Col>
+            </Row>
+
+            <Row justify={"center"}>
+                <Col xs={24} md={8}>
+                    <Form.Item
+                        name="email"
+                        label="Email"
+                        rules={[
+                            {
+                                required: true,
+                                message: "Please enter your email!"
+                            },
+                            {
+                                type: "email",
+                                message: "Email is not in correct format"
+                            }
+                        ]}
+                    >
+                        <Input />
+                    </Form.Item>
+                </Col>
+            </Row>
+
+            <Row justify={"center"}>
+                <Col xs={24} md={8}>
+                    <Form.Item
+                        name="password"
+                        label="Password"
+                        rules={[
+                            {
+                                required: true,
+                                message: "Please enter your password!"
+                            },
+                        ]}
+                    >
+                        <Input />
+                    </Form.Item>
+                </Col>
+            </Row>
+
+            <Row justify={"center"}>
+                <Col xs={24} md={8}>
+                    <Form.Item
+                        name="phone"
+                        label="Phone number"
+                        rules={[
+                            {
+                                required: true,
+                                message: "Please enter your phone number!"
+                            },
+                            {
+                                required: true,
+                                pattern: new RegExp(/\d+/g),
+                                message: "Phone number is not in correct format!"
+                            }
+                        ]}
+                    >
+                        <Input />
+                    </Form.Item>
+                </Col>
+            </Row>
+
+            <Row justify={"center"}>
+                <Col xs={24} md={8}>
+                    <div>
+                        <Button type="primary" onClick={() => { form.submit() }}>Register</Button>
+                    </div>
+                    <Divider />
+                    <div>Đã có tài khoản? <Link to={"/login"}>Đăng nhập tại đây</Link></div>
+                </Col>
+            </Row>
+        </Form>
     )
 }
-
 export default RegisterPage
